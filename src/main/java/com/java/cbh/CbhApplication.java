196fdecb27e64f837591a052598371198ad2f5ac
ChenLu1051196308 <1051196308@qq.com>
2022-01-11 19:17:27 +0800

    2.2

---
+++ src/main/java/com/java/cbh/CbhApplication.java
@@ -0,0 +1,13 @@
+package com.java.cbh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/cbh/CbhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.cbh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
