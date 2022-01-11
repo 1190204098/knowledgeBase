c477d09dd7245821c3a782b849c1bdf4da2560ab
ChenLu1051196308 <1051196308@qq.com>
2022-01-11 19:50:30 +0800

    2.5

--- src/main/java/com/java/cbh/CbhApplication.java
+++ src/main/java/com/java/cbh/CbhApplication.java
@@ -1,13 +1,26 @@
 package com.java.cbh;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class CbhApplication {
 
+	private static final Logger LOG = LoggerFactory.getLogger(CbhApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(CbhApplication.class, args);
+
+		SpringApplication app = new SpringApplication(CbhApplication
+				.class);
+		Environment env = app.run(args).getEnvironment();
+		LOG.info("启动成功!! ");
+		LOG.info("地址: \thttp://127.0.0.1:{}", env.getProperty
+				("server.port"));
+
+
 	}
 
 }
